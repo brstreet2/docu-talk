e4fcca81f2d7890fea88d332477d20fbfb01aaf6
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-11-25 19:02:51 +0700

    Update MessagesWrapper.tsx

--- src/components/dashboard/messages/MessagesWrapper.tsx
+++ src/components/dashboard/messages/MessagesWrapper.tsx
@@ -6,6 +6,7 @@ import Messages from "./Messages";
 import { ChevronLeft, Loader2, XCircle } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { MessageContextProvider } from "./MessageContext";
 
 interface MessagesWrapperProps {
   fileId: string;
@@ -83,13 +84,15 @@ const MessagesWrapper = ({ fileId }: MessagesWrapperProps) => {
     );
 
   return (
-    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
-      <div className="flex-1 justify-between flex flex-col mb-28">
-        <Messages />
-      </div>
+    <MessageContextProvider fileId={fileId}>
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 justify-between flex flex-col mb-28">
+          <Messages />
+        </div>
 
-      <InputMessage />
-    </div>
+        <InputMessage />
+      </div>
+    </MessageContextProvider>
   );
 };
 
