2f0b8ed9ba9055b6b1abd9f153cbb81d65a314ed
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-11-25 23:16:05 +0700

    feat(chat/messages): Working chat messages feature

--- src/components/dashboard/messages/InputMessage.tsx
+++ src/components/dashboard/messages/InputMessage.tsx
@@ -16,7 +16,7 @@ const InputMessage = ({ isDisabled }: InputMessageProps) => {
 
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-5 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-5 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4">
             <div className="relative">
@@ -44,7 +44,6 @@ const InputMessage = ({ isDisabled }: InputMessageProps) => {
                 disabled={isLoading || isDisabled}
                 aria-label="send message"
                 className="absolute bottom-1.5 right-[8px]"
-                type="submit"
                 onClick={() => {
                   addMessage();
 
@@ -56,7 +55,7 @@ const InputMessage = ({ isDisabled }: InputMessageProps) => {
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
