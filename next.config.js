46ea51bc1aa76ca505194f1de1e76a9787c53505
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-11-29 14:22:38 +0700

    feat(next config): Wild card any hostname from googleusercontent (avatar image)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
         config.resolve.alias.canvas = false
         config.resolve.alias.encoding = false
         return config
-    }
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "*.googleusercontent.com",
+            port: "",
+            pathname: "**",
+          },
+        ],
+    },
 }
 
 module.exports = nextConfig
