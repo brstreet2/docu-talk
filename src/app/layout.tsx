0cda881b1b502f0ffa9157c972bce6b8b7f1a11f
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-11-29 14:21:16 +0700

    Refactor(navbar): Import from the correct path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/nav/Navbar";
 import Providers from "@/components/Providers";
 import "react-loading-skeleton/dist/skeleton.css";
 import { Toaster } from "@/components/ui/toaster";
