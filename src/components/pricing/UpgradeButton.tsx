b9ebcf6dd148b80bc187d165c3557373ee87c1d0
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-02-18 12:24:36 +0700

    fix(upgrade button): change from using redirect to router

--- src/components/pricing/UpgradeButton.tsx
+++ src/components/pricing/UpgradeButton.tsx
@@ -4,7 +4,7 @@ import { ArrowRight, Loader2 } from "lucide-react";
 import { Button } from "../ui/button";
 import { trpc } from "@/app/_trpc/client";
 import { useState } from "react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useToast } from "../ui/use-toast";
 
 interface UpgradeButtonProps {
@@ -12,6 +12,7 @@ interface UpgradeButtonProps {
 }
 
 const UpgradeButton = ({ memberType }: UpgradeButtonProps) => {
+  const router = useRouter();
   const { toast } = useToast();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { mutate: createXenditSession } = trpc.createXenditSession.useMutation({
@@ -19,15 +20,15 @@ const UpgradeButton = ({ memberType }: UpgradeButtonProps) => {
       setIsLoading(true);
     },
     onSuccess: (response) => {
-      redirect("/dashboard/subscription");
+      router.push("/dashboard/subscription");
     },
     onError: (response) => {
+      console.log(response);
       toast({
         title: "There was a problem...",
         description: "Please try again in a moment",
         variant: "destructive",
       });
-      console.log(response);
       setIsLoading(false);
     },
   });
